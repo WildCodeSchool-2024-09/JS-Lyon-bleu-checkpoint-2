b7a3d8bf9aa53f726b9245c4955f5af2b46ca3df
STEPHEN BACHER <bacher.stephen@gmail.com>
2024-12-02 12:00:20 +0100

    Step 3/5 commit

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
-// biome-ignore lint/suspicious/noRedeclare: <explanation>
-interface Cupcake {
+interface Accessory {
+  id: number;
+  name: string;
+  slug: string;
+}
+interface CupcakeList {
   id: number;
   accessory_id: string;
   accessory: string;
@@ -14,11 +18,11 @@ interface Cupcake {
 
 function CupcakeList() {
   // Step 1: get all cupcakes (with useEffect)
-  const [cupcakes, setCupcakes] = useState<Cupcake[]>([]);
+  const [cupcakes, setCupcakes] = useState<CupcakeList[]>([]);
 
   // Step 3: get all accessories
-
-  // Step 5: create filter state
+  const [accessories, setAccessories] = useState<Accessory[]>([]);
+  const [selectedAccessory, setSelectedAccessory] = useState<string>("");
 
   useEffect(() => {
     fetch("http://localhost:3310/api/cupcakes")
@@ -28,19 +32,46 @@ function CupcakeList() {
       });
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:3310/api/accessories")
+      .then((res) => res.json())
+      .then((data) => {
+        setAccessories(data);
+      });
+  }, []);
+
+  const handleAccessoryFilter = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSelectedAccessory(event.target.value);
+  };
+
+  const Cupcakesfiltered = selectedAccessory
+    ? cupcakes.filter((cupcake) => cupcake.accessory_id === selectedAccessory)
+    : cupcakes;
+
   return (
     <>
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            value={selectedAccessory}
+            onChange={handleAccessoryFilter}
+          >
             <option value="">---</option>
+            {accessories.map((accessory) => (
+              <option key={accessory.id} value={accessory.id.toString()}>
+                {accessory.name}
+              </option>
+            ))}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {cupcakes.map((cupcake) => (
+        {Cupcakesfiltered.map((cupcake) => (
           <li key={cupcake.id} className="cupcake-item">
             <Cupcake data={cupcake} />
           </li>
