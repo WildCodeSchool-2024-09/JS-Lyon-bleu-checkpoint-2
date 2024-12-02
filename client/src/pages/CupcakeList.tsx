91bb142373fd00d03b3f888819c636dfc2e04c5e
TheophileSimoes <theophile.sdn@gmail.com>
2024-12-02 11:16:05 +0100

    step 3 et 4

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,36 +1,53 @@
 import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
+interface accessoryArrayProps {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+interface CupcakeProps {
+  data: {
+    id: number;
+    accessory_id: string;
+    accessory: string;
+    color1: string;
+    color2: string;
+    color3: string;
+    name: string;
+  }[];
+}
 /* ************************************************************************* */
-const sampleCupcakes = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
+// const sampleCupcakes = [
+// 	{
+// 		id: 10,
+// 		accessory_id: "4",
+// 		accessory: "wcs",
+// 		color1: "blue",
+// 		color2: "white",
+// 		color3: "red",
+// 		name: "France",
+// 	},
+// 	{
+// 		id: 11,
+// 		accessory_id: "4",
+// 		accessory: "wcs",
+// 		color1: "yellow",
+// 		color2: "red",
+// 		color3: "black",
+// 		name: "Germany",
+// 	},
+// 	{
+// 		id: 27,
+// 		accessory_id: "5",
+// 		accessory: "christmas-candy",
+// 		color1: "yellow",
+// 		color2: "blue",
+// 		color3: "blue",
+// 		name: "Sweden",
+// 	},
+// ];
 
 // type CupcakeArray = typeof sampleCupcakes;
 
@@ -39,18 +56,21 @@ const sampleCupcakes = [
 /* ************************************************************************* */
 
 function CupcakeList() {
-  const [cupcakes, setCupcakes] = useState([]);
+  // Step 1: get all cupcakes (with useEffect)
+  const [cupcakes, setCupcakes] = useState<CupcakeProps["data"]>([]);
   useEffect(() => {
     fetch("http://localhost:3310/api/cupcakes")
       .then((res) => res.json())
-      .then((cake) => {
-        setCupcakes(cake);
-      });
+      .then((cake) => setCupcakes(cake));
   }, []);
 
-  // Step 1: get all cupcakes (with useEffect)
-
   // Step 3: get all accessories
+  const [accessories, setAccessories] = useState<accessoryArrayProps[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/api/accessories")
+      .then((res) => res.json())
+      .then((topping) => setAccessories(topping));
+  });
 
   // Step 5: create filter state
 
@@ -63,18 +83,19 @@ function CupcakeList() {
           Filter by{" "}
           <select id="cupcake-select">
             <option value="">---</option>
+            {accessories.map((accessory) => (
+              <option key={accessory.id}>{accessory.name}</option>
+            ))}
             {/* Step 4: add an option for each accessory */}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {cupcakes.map((cakes) => (
-          <Cupcake data={cakes} key={cakes} />
+          <Cupcake data={cakes} key={cakes.id} />
         ))}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
+        {/* <li className="cupcake-item"></li> */}
         {/* end of block */}
       </ul>
     </>
